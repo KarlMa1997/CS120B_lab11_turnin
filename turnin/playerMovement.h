2507a6ce21572ee020527296a809ac8d27b4ca5e
KarlMa1997 <kaihangma@gmail.com>
2019-12-03 14:37:37 -0800

    Update playerMovement.h

--- turnin/playerMovement.h
+++ turnin/playerMovement.h
@@ -4,8 +4,8 @@
 #include "customChar.h"
 #include "io.h" 
 
-#define R (~PINA & 0x10) // right
-#define D (~PINA & 0x04) // duck, or down
+#define R (~PINA & 0x10) 
+#define D (~PINA & 0x04) 
 #define ROW1_BEGIN 1
 #define ROW2_BEGIN 17
 
@@ -23,9 +23,7 @@ enum MovementFlag
 enum PlayerMovementStates
 {
     WAIT,
-//    MOVE_RIGHT, // moves player over once
-//    SCROLL_RIGHT, // continues to scroll player
-    DUCK // for dodging, ducking, dipping, diving, and dodging
+    DUCK 
 };
 
 void spriteUpdate(unsigned char currSprite)
@@ -39,18 +37,12 @@ int playerMovementTick(int state)
     static unsigned long count = 0;
     static unsigned char sprite = 0; 
 
-    // transistions
     switch(state)
     {
         case WAIT:
         {
             if(inMenu == 0)
             {
-                /*
-                if(R && !D) 
-                {
-                    state = MOVE_RIGHT; 
-                } */
                 if(D)
                 {
                     state = DUCK; 
@@ -65,33 +57,7 @@ int playerMovementTick(int state)
             }
             break;
         }
-/*        case MOVE_RIGHT:
-        {
-            if(R && !D) 
-            {
-                state = SCROLL_RIGHT; 
-            }
-            else
-            {
-                state = WAIT;
-            }
-            break;
-        }
-        case SCROLL_RIGHT:
-        {
-            if(R && !D) 
-            {
-                state = SCROLL_RIGHT; 
-            }
-            else
-            {
-                // character must not be moving to duck, so if the player
-                // presses duck while moving, the character will stop moving
-                // first and the player will have to press duck again
-                state = WAIT; 
-            }
-            break;
-        } */
+
         case DUCK:
         {
             if(D) 
@@ -101,8 +67,7 @@ int playerMovementTick(int state)
             else
             {
                 state = WAIT;
-                
-                // clear the bottom row character and repaint on top row
+           
                 LCD_Cursor(ROW2_BEGIN);
                 LCD_WriteData(' ');
                 LCD_Cursor(ROW1_BEGIN);
@@ -113,41 +78,18 @@ int playerMovementTick(int state)
         }
     }
 
-    // actions
     switch(state)
     {
         case WAIT: 
         {
             count = 0; 
-//            walking = FALSE;
             ducking = FALSE;
             
             break;
         }
-/*        case MOVE_RIGHT:
-        {
-            walking = TRUE;
-            ducking = FALSE;
-            spriteUpdate(sprite);
-//            scrollUpdate();
-            break;
-        }
-        case SCROLL_RIGHT:
-        {
-            walking = TRUE;
-            ducking = FALSE;
-            count++; 
-            if(count == ms250)
-            {
-                count = 0;
-                spriteUpdate(sprite);
-//                scrollUpdate();
-            }
-            break;
-        } */
+
         case DUCK:
         {
-//            walking = FALSE;
             ducking = TRUE;
             
             LCD_Cursor(ROW2_BEGIN);
