3689806ffe865de9b83092fcbc20e2fe0e1c8275
KarlMa1997 <kaihangma@gmail.com>
2019-12-03 14:35:57 -0800

    Update playeShots.h

--- turnin/playeShots.h
+++ turnin/playeShots.h
@@ -6,14 +6,13 @@
 
 #define shoot (~PINA & 0x02)
 
-#define BULLET_SPACE 15 // max blocks bullet can travel
+#define BULLET_SPACE 15
 
-// amount of bullet space when monsters are present
 unsigned char activeBulletSpace = BULLET_SPACE; 
 
 unsigned char playerBullets[BULLET_SPACE + 1] = "               ";
 unsigned char shot;
-unsigned char potentialHit; // keep track of actual hits
+unsigned char potentialHit; 
 
 enum PlayerShootStates
 {
@@ -24,7 +23,6 @@ enum PlayerShootStates
 
 int playerShootTick(int state)
 {
-    // transitions
     switch(state)
     {
         case WAIT_SHOT: 
@@ -62,16 +60,11 @@ int playerShootTick(int state)
         }
         case PLAYER_SHOT:
         {
-//            shot = 1; 
-//            potentialHit = 1; 
-//            state = WAIT_SHOT;
             state = shoot ? PLAYER_SHOT : WAIT_SHOT; 
-            
             break;
         }
     }
 
-    // actions
     switch(state)
     {
         case WAIT_SHOT: shot = 0; break;
@@ -90,7 +83,6 @@ enum UpdateShotsStates
 
 int updateShotsTick(int state)
 {
-    // transitions
     switch(state)
     {
         case UPDATE_SHOTS: 
@@ -122,7 +114,6 @@ int updateShotsTick(int state)
         case SHOT_BTN_HELD: state = shot ? SHOT_BTN_HELD : UPDATE_SHOTS; break;
     }
 
-    // actions
     switch(state)
     {
         case UPDATE_SHOTS:
