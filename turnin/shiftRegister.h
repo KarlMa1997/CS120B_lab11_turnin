916fc5ebcfc0e68ae845fe240e623bc52b6f478e
KarlMa1997 <kaihangma@gmail.com>
2019-12-03 14:39:51 -0800

    Update shiftRegister.h

--- turnin/shiftRegister.h
+++ turnin/shiftRegister.h
@@ -6,13 +6,12 @@
 
 #define PORT PORTB  
 #define DDR DDRB
-#define DATA PB0            // data pin
-#define SHIFT_CLK PB1       // shift clock
-#define STORE_CLK PB2       // store block
+#define DATA PB0            
+#define SHIFT_CLK PB1      
+#define STORE_CLK PB2     
 
 void shiftInit()
 {
-    // init DDR
     DDR |= ((1 << SHIFT_CLK) | (1 << STORE_CLK) | (1 << DATA));
 }
 
@@ -26,44 +25,42 @@ void shiftLow()
     PORT &= (~(1 << DATA));
 }
 
-// pulse the shift clock
 void shiftPulse()
 {
     PORT |= (1 << SHIFT_CLK);
     PORT &= (~(1 << SHIFT_CLK));
 }
 
-// pulse the store clock
 void shiftLatch()
 {
     PORT |= (1 << STORE_CLK);
-    _delay_loop_1(1); // delay for data storage
+    _delay_loop_1(1); 
     PORT &= (~(1 << STORE_CLK));
     _delay_loop_1(1);
 }
 
 void shiftWrite(uint8_t data)
 {
-    // send all 8 bits one at a time
+
     for(uint8_t i = 0; i < 8; i++)
     {
-        // output data according to MSB
+
         if(data & 0b10000000)
         {
-            // MSB is one
+           
             shiftHigh();
         }
         else
         {              
-            // MSB is zero
+          
             shiftLow();
         }
 
-        shiftPulse();  // pulse shift clock
-        data = data << 1;  // shift MSB
+        shiftPulse();  
+        data = data << 1;  
     }
 
-    // send bits to output latch
+
     shiftLatch();
 }
 
